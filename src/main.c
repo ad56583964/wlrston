8238639f17882afcb2ffc0169d6816463ac885b8
He Yong <hyyoxhk@163.com>
2024-11-21 17:36:13 +0800

    Implement plugin mode
    Signed-off-by: He Yong <hyyoxhk@163.com>

--- src/main.c
+++ src/main.c
@@ -1,15 +1,21 @@
 #define _POSIX_C_SOURCE 200112L
+
+#include "config.h"
+
 #include <assert.h>
 #include <getopt.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
 #include <wayland-server-core.h>
 #include <wlr/util/log.h>
 #include <xkbcommon/xkbcommon.h>
 #include <signal.h>
+#include <linux/limits.h>
+#include <dlfcn.h>
 
 #include <wlrston.h>
 
@@ -26,6 +32,105 @@ static int on_term_signal(int signal_number, void *data)
 static void sigint_helper(int sig)
 {
 	raise(SIGUSR2);
+}
+
+static size_t
+module_path_from_env(const char *name, char *path, size_t path_len)
+{
+	const char *mapping = getenv("WLRSTON_MODULE_MAP");
+	const char *end;
+	const int name_len = strlen(name);
+
+	if (!mapping)
+		return 0;
+
+	end = mapping + strlen(mapping);
+	while (mapping < end && *mapping) {
+		const char *filename, *next;
+
+		/* early out: impossibly short string */
+		if (end - mapping < name_len + 1)
+			return 0;
+
+		filename = &mapping[name_len + 1];
+		next = strchrnul(mapping, ';');
+
+		if (strncmp(mapping, name, name_len) == 0 &&
+		    mapping[name_len] == '=') {
+			size_t file_len = next - filename; /* no trailing NUL */
+			if (file_len >= path_len)
+				return 0;
+			strncpy(path, filename, file_len);
+			path[file_len] = '\0';
+			return file_len;
+		}
+
+		mapping = next + 1;
+	}
+
+	return 0;
+}
+
+
+static void *
+load_module_entrypoint(const char *name, const char *entrypoint, const char *module_dir)
+{
+	char path[PATH_MAX];
+	void *module, *init;
+	size_t len;
+
+	if (name == NULL)
+		return NULL;
+
+	if (name[0] != '/') {
+		len = module_path_from_env(name, path, sizeof path);
+		if (len == 0)
+			len = snprintf(path, sizeof path, "%s/%s",
+				       module_dir, name);
+	} else {
+		len = snprintf(path, sizeof path, "%s", name);
+	}
+
+	/* snprintf returns the length of the string it would've written,
+	 * _excluding_ the NUL byte. So even being equal to the size of
+	 * our buffer is an error here. */
+	if (len >= sizeof path)
+		return NULL;
+
+	module = dlopen(path, RTLD_NOW | RTLD_NOLOAD);
+	if (module) {
+		wlr_log(WLR_ERROR, "Module '%s' already loaded\n", path);
+	} else {
+		wlr_log(WLR_ERROR, "Loading module '%s'\n", path);
+		module = dlopen(path, RTLD_NOW);
+		if (!module) {
+			wlr_log(WLR_ERROR, "Failed to load module: %s\n", dlerror());
+			return NULL;
+		}
+	}
+
+	init = dlsym(module, entrypoint);
+	if (!init) {
+		wlr_log(WLR_ERROR, "Failed to lookup init function: %s\n", dlerror());
+		dlclose(module);
+		return NULL;
+	}
+
+	return init;
+}
+
+static int
+load_shell(struct wlrston_server *server, const char *name, int *argc, char *argv[])
+{
+	int (*shell_init)(struct wlrston_server *server,
+			  int *argc, char *argv[]);
+
+	shell_init = load_module_entrypoint(name, "wlrston_shell_init", SHELLDIR);
+	if (!shell_init)
+		return -1;
+	if (shell_init(server, argc, argv) < 0)
+		return -1;
+	return 0;
 }
 
 int main(int argc, char *argv[])
@@ -80,20 +185,16 @@ int main(int argc, char *argv[])
 		goto out_signals;
 	}
 
+	if (!wlr_backend_start(server->backend))
+		goto out;
+
 	/* Add a Unix socket to the Wayland display. */
 	const char *socket = wl_display_add_socket_auto(display);
-	if (!socket) {
-		wlr_backend_destroy(server->backend);
-		return 1;
-	}
+	if (!socket)
+		goto out;
 
-	/* Start the backend. This will enumerate outputs and inputs, become the DRM
-	 * master, etc */
-	if (!wlr_backend_start(server->backend)) {
-		wlr_backend_destroy(server->backend);
-		wl_display_destroy(display);
-		return 1;
-	}
+	// load_shell
+	load_shell(server, "desktop-shell.so", &argc, argv);
 
 	setenv("WAYLAND_DISPLAY", socket, true);
 	if (startup_cmd) {
@@ -106,6 +207,7 @@ int main(int argc, char *argv[])
 			socket);
 	wl_display_run(display);
 
+out:
 	server_destory(server);
 
 out_signals:
